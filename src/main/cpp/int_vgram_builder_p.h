c3642c8a9901e164a8021458866b8af494e66afd
akhvorov <akhvorov@yandex-team.ru>
2018-08-12 16:40:33 +0300

    directives in headers renamed [2]

--- src/main/cpp/int_vgram_builder_p.h
+++ src/main/cpp/int_vgram_builder_p.h
@@ -2,8 +2,8 @@
 // Created by Igor E. Kuralenok on 13.07.2018.
 //
 
-#ifndef VGRAM_INT_VGRAM_BUILDER_H
-#define VGRAM_INT_VGRAM_BUILDER_H
+#ifndef VGRAM_INT_VGRAM_BUILDER_P_H
+#define VGRAM_INT_VGRAM_BUILDER_P_H
 
 #include "int_vgram_builder.h"
 
@@ -12,4 +12,4 @@ class IntVGramBuilderImpl : public IntVGramBuilder {
 };
 
 
-#endif //VGRAM_INT_VGRAM_BUILDER_H
+#endif //VGRAM_INT_VGRAM_BUILDER_P_H
