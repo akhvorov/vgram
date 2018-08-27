42d5774e58065b3571695815148e76eb77874d73
akhvorov <khvorov.aleksandr@gmail.com>
2018-08-27 21:56:51 +0300

    move library to src

--- src/main/cpp/library.cc
+++ src/main/cpp/library.cc
@@ -2,7 +2,7 @@
 #include <boost/python/def.hpp>
 #include <boost/python.hpp>
 #include <vector>
-#include "src/test/cpp/ListDictionaryTest.h"
+#include "../../test/cpp/ListDictionaryTest.h"
 //#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 
