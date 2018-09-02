945928971fe98784929a79f6d5dbe8387ae277a0
akhvorov <khvorov.aleksandr@gmail.com>
2018-09-02 12:54:59 +0300

    add common tests file

---
+++ src/test/cpp/common.cc
@@ -0,0 +1,18 @@
+//
+// Created by Aleksandr Khvorov on 02.09.18.
+//
+
+#include "common.h"
+
+std::vector<std::vector<int>> simple_seqs() {
+    std::vector<std::vector<int>> seqs;
+    int seq1[] = {0, 1}; // 1
+    int seq2[] = {0}; // 0
+    int seq3[] = {1}; // 3
+    int seq4[] = {0, 1, 1}; // 2
+    seqs.emplace_back(std::vector<int>(std::begin(seq1), std::end(seq1)));
+    seqs.emplace_back(std::vector<int>(std::begin(seq2), std::end(seq2)));
+    seqs.emplace_back(std::vector<int>(std::begin(seq3), std::end(seq3)));
+    seqs.emplace_back(std::vector<int>(std::begin(seq4), std::end(seq4)));
+    return seqs;
+}
---
+++ src/test/cpp/common.h
@@ -0,0 +1,12 @@
+//
+// Created by Aleksandr Khvorov on 02.09.18.
+//
+
+#ifndef DICT_EXPANSION_COMMON_H
+#define DICT_EXPANSION_COMMON_H
+
+#include <vector>
+
+std::vector<std::vector<int>> simple_seqs();
+
+#endif //DICT_EXPANSION_COMMON_H
