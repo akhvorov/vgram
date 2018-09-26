f9312e70275415e3ca34213b76f4b4c0068e1cfb
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2018-09-26 14:28:05 +0300

    test on synthetic data passed

--- src/test/cpp/int_vgram_builder_test.cpp
+++ src/test/cpp/int_vgram_builder_test.cpp
@@ -3,9 +3,11 @@
 //
 
 #include <gtest/gtest.h>
-#include "../../main/cpp/int_vgram_builder_p.h"
-#include "common.h"
 #include <random>
+#include "../../main/cpp/int_vgram_builder_p.h"
+#include "../../main/cpp/int_dict_p.h"
+#include "common.h"
+#include "../../main/cpp/vector_hash.h"
 
 TEST(IntVgramBuilderTests, EmptyTest) {
     ASSERT_TRUE(true);
@@ -28,7 +30,7 @@ TEST(IntVgramBuilderTests, IndependenceTest) {
     for (char a = 'a'; a <= 'z'; a++)
         alpha.push_back(a);
     IntVGramBuilder* vgb = new IntVGramBuilderImpl(alpha, (int)alpha.size() + 100);
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 10000; i++) {
         int len = rand_len(generator);
         std::vector<int> builder;
         builder.reserve(len);
@@ -37,4 +39,110 @@ TEST(IntVgramBuilderTests, IndependenceTest) {
         vgb->accept(builder);
     }
     ASSERT_TRUE('z' - 'a' + 5 > vgb->result()->size());
+}
+
+TEST(IntVGramBuilderTests, RestoreTest) {
+    std::vector<std::vector<int>> seqs;
+    seqs.push_back(std::vector<int>{0});
+    seqs.push_back(std::vector<int>{1});
+    seqs.push_back(std::vector<int>{2});
+    seqs.push_back(std::vector<int>{2, 2});
+    seqs.push_back(std::vector<int>{0, 0});
+    seqs.push_back(std::vector<int>{1, 1});
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> rand_len(1, 30);
+    std::uniform_int_distribution<int> sample(0, static_cast<int>(seqs.size()));
+    IntDict* dict = new IntDictImpl(seqs);
+
+    bool equalsAtLeastOnce = false;
+    for (int i = 0; i < 10 && !equalsAtLeastOnce; i++) {
+        IntSeq alpha;
+        for (int a = 0; a <= 2; a++) {
+            alpha.push_back(a);
+        }
+        IntVGramBuilder* de = new IntVGramBuilderImpl(alpha, dict->size());
+        for (int j = 0; j < 1000; j++) {
+            int len = rand_len(generator);
+            IntSeq seq;
+            for (int c = 0; c < len; c++) {
+                int ind = sample(generator);
+                seq.insert(seq.end(), seqs[ind].begin(), seqs[ind].end());
+            }
+            de->accept(seq);
+        }
+        equalsAtLeastOnce = de->result()->alphabet() == dict->alphabet();
+    }
+    ASSERT_TRUE(equalsAtLeastOnce);
+}
+
+TEST(IntVGramBuilderTests, RestoreTestAsym) {
+    std::vector<std::vector<int>> seqs;
+    seqs.push_back(std::vector<int>{0});
+    seqs.push_back(std::vector<int>{1});
+    seqs.push_back(std::vector<int>{2});
+    seqs.push_back(std::vector<int>{2, 2});
+    seqs.push_back(std::vector<int>{0, 1});
+    seqs.push_back(std::vector<int>{1, 1});
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> rand_len(1, 30);
+    std::uniform_int_distribution<int> sample(0, static_cast<int>(seqs.size()) - 1);
+    IntDict* dict = new IntDictImpl(seqs);
+
+    bool equalsAtLeastOnce = false;
+    std::cout << std::endl;
+    for (int i = 0; i < 10 && !equalsAtLeastOnce; i++) {
+        IntSeq alpha;
+        for (int a = 0; a <= 2; a++) {
+            alpha.push_back(a);
+        }
+        IntVGramBuilder* de = new IntVGramBuilderImpl(alpha, dict->size());
+        for (int j = 0; j < 5000; j++) {
+            int len = rand_len(generator);
+            IntSeq seq;
+            for (int c = 0; c < len; c++) {
+                int ind = sample(generator);
+                seq.insert(seq.end(), seqs[ind].begin(), seqs[ind].end());
+            }
+            de->accept(seq);
+        }
+        equalsAtLeastOnce = de->result()->alphabet() == dict->alphabet();
+    }
+    ASSERT_TRUE(equalsAtLeastOnce);
+}
+
+TEST(IntVGramBuilderTests, RestoreTestLong) {
+    std::vector<IntSeq> seqs;
+    seqs.push_back(std::vector<int>{4, 0, 1, 0});
+    seqs.push_back(std::vector<int>{2, 0, 3, 0, 2});
+    seqs.push_back(std::vector<int>{0, 1, 0, 0});
+    seqs.push_back(std::vector<int>{1, 0, 1});
+    seqs.push_back(std::vector<int>{2, 0, 1, 3, 0, 2, 0, 4, 0, 1, 3, 0});
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> rand_len(1, 100);
+    std::uniform_int_distribution<int> sample(0, static_cast<int>(seqs.size()) - 1);
+    IntDict* dict = new IntDictImpl(seqs);
+
+    bool equalsAtLeastOnce = false;
+    std::cout << std::endl;
+    for (int i = 0; i < 1 && !equalsAtLeastOnce; i++) {
+        IntDict* alpha = new IntDictImpl(std::vector<IntSeq>{{0}, {1}, {2}, {3}, {4}});
+        IntVGramBuilder* de = new IntVGramBuilderImpl(*alpha, dict->size() + alpha->size());
+        for (int j = 0; j < 1000; j++) {
+            int len = rand_len(generator);
+            IntSeq seq;
+            for (int c = 0; c < len; c++) {
+                int ind = sample(generator);
+                seq.insert(seq.end(), seqs[ind].begin(), seqs[ind].end());
+            }
+            de->accept(seq);
+        }
+        for (const IntSeq& alpha_seq : alpha->alphabet())
+            seqs.push_back(alpha_seq);
+        std::sort(seqs.begin(), seqs.end());
+        equalsAtLeastOnce = de->result()->alphabet() == seqs;
+    }
+    ASSERT_TRUE(equalsAtLeastOnce);
 }
