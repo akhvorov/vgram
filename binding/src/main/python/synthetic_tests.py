dace41af3eb1d6081a2aecf508105dfb0e01fc9e
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2018-10-04 19:01:23 +0300

    four modes, two datasets and one error

--- binding/src/main/python/synthetic_tests.py
+++ binding/src/main/python/synthetic_tests.py
@@ -1,5 +1,8 @@
 import random
 import time
+import re
+import os
+import argparse
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
@@ -26,162 +29,85 @@ def download_data(cats=None):
     return fetch_20newsgroups(subset='train', categories=cats), fetch_20newsgroups(subset='test', categories=cats)
 
 
-def classify_text(pipeline):
-    train, test = download_data()
-
-    pipeline = pipeline.fit(train.data, train.target)
-
-    docs_test = test.data
-    predicted = pipeline.predict(docs_test)
-    print(np.mean(predicted == test.target))
-
-
-def newsgroups1():
-    n_features = 20000
-    print('num of features:', n_features)
-    cats = None
-    baseline = Pipeline([('vect', CountVectorizer()),
-                         ('tfidf', TfidfTransformer()),
-                         # ('kbest', SelectKBest(k=n_features)),
-                         (('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)))
-                         ])
-    '''vgramm = Pipeline([('vect', CountVectorizer(vocabulary=voc)),
-                       ('tfidf', TfidfTransformer()),
-                       ('kbest', SelectKBest(k=n_features)),
-                       (('clf', LinearSVC()))
-                       ])'''
-    classify_text(baseline)
+def imdb_data(directory):
+    # labels = set([x for x in os.listdir(directory) if os.path.isdir(x)])
+    # if 'unsup' in labels:
+    #     labels.remove('unsup')
+    # labels = list(labels)
+    # labels.sort()
+    labels = ["neg", "pos"]
+    lab_to_num = {}
+    for i, l in enumerate(labels):
+        lab_to_num[l] = i
+    X, y = [], []
+    for l in labels:
+        path = directory + l
+        for filename in os.listdir(path):
+            # if '(' in filename:
+            #     continue
+            f = open(path + '/' + filename, encoding='latin1')
+            X.append(f.read())
+            f.close()
+            y.append(lab_to_num[l])
+    return X, y
 
 
-class MyTokenizer:
+class BaseTokenizer:
     def __init__(self):
         self.fcode = {}
         self.bcode = {}
-        self.size = 0
 
-    # def encode(self, doc, sep=' '):
-    #     seq = []
-    #     for word in doc.split(sep):
-    #         if word not in map:
-    #             self.fcode[word] = self.size
-    #             self.bcode[self.size] = word
-    #             self.size += 1
-    #         seq.append(self.fcode[word])
-    #     return seq
+    def fit(self, X, y=None):
+        xx = self.normalize(X)
+        xx = self.tokenize(xx)
+        tokens = set()
+        for x in xx:
+            for w in x:
+                tokens.add(w)
+        tokens = sorted(list(tokens))
+        s = 0
+        for token in tokens:
+            self.fcode[token] = s
+            self.bcode[s] = token
+            s += 1
+        print("tokenizer dict len: ", len(self.fcode))
+        return self
 
-    def encode(self, docs, sep=''):
-        res = []
-        for doc in docs:
-            seq = []
-            if sep == '':
-                words = doc
-            else:
-                words = doc.split(sep)
-            for word in words:
-                if word not in self.fcode:
-                    self.fcode[word] = self.size
-                    self.bcode[self.size] = word
-                    self.size += 1
-                seq.append(self.fcode[word])
-            res.append(seq)
-        return res
+    def transform(self, X, y=None):
+        xx = self.normalize(X)
+        xx = self.tokenize(xx)
+        return [[self.fcode[w] for w in x if w in self.fcode.keys()] for x in xx]
 
-    def decode(self, docs):
-        res = []
-        for doc in docs:
-            seq = ""
-            for symb in doc:
-                seq += self.bcode[symb]
-            res.append(seq)
-        return res
+    def decode(self, X, y=None):
+        return ["".join([self.bcode[w] for w in x if w in self.bcode.keys()]) for x in X]
 
-    def coded_format(self, seqs):
-        res = []
-        for seq in seqs:
-            doc = ' '.join(['_'.join([str(i) for i in word]) for word in seq])
-            res.append(doc)
-        return res
+    def normalize(self, X):
+        return [re.sub("[^\w\d]", "", x).lower() for x in X]
+
+    def tokenize(self, X):
+        return [[c for c in x] for x in X]
 
 
-def alpha_info(vgb, tokenizer):
-    alpha0 = vgb.alphabet()
-    print("alpha size: ", len(alpha0))
-    alpha = tokenizer.decode(alpha0)
-    for i in range(1000):
-        print(alpha0[i], " -- ", alpha[i])
+class WordTokenizer(BaseTokenizer):
+    def __init__(self):
+        super().__init__()
+
+    def normalize(self, X):
+        return [re.sub("[^ \w\d]", "", re.sub(" +", " ", x)).lower() for x in X]
+
+    def tokenize(self, X):
+        return [x.split(" ") for x in X]
 
 
-def do_ml(X_tr, y_tr, X_te, y_te):
-    word_vectorizer1 = CountVectorizer()
-    word_vectorizer1.fit(X_tr)
-    pipeline = Pipeline([('vect', CountVectorizer(vocabulary=word_vectorizer1.vocabulary)),
-                         ('tfidf', TfidfTransformer()),
-                         (('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=5, random_state=42)))
-                         ])
-    pipeline = pipeline.fit(X_tr, y_tr)
-    predicted = pipeline.predict(X_te)
-    print(np.mean(predicted == y_te))
+class CharTokenizer(BaseTokenizer):
+    def __init__(self):
+        super().__init__()
 
+    def normalize(self, X):
+        return [re.sub("[^\w\d]", "", x).lower() for x in X]
 
-def test_20ng():
-    start = time.time()
-    train, test = fetch_20newsgroups(subset='train'), fetch_20newsgroups(subset='test')
-    print("dataset: ", time.time() - start)
-    tokenizer = MyTokenizer()
-    ttrain = tokenizer.encode(train.data)
-    ttest = tokenizer.encode(test.data)
-    print("encode: ", time.time() - start)
-    vgb = VGramBuilder(1000)
-    for k in range(10):
-        for i in range(len(ttrain)):
-            vgb.accept(ttrain[i])
-        for i in range(len(ttest)):
-            vgb.accept(ttest[i])
-    print("accept: ", time.time() - start)
-
-    #alpha_info(vgb, tokenizer)
-    alpha = vgb.alphabet()
-
-    ttrain_v = tokenizer.coded_format([[alpha[i] for i in vgb.parse(doc) if i < len(alpha)] for doc in ttrain])
-    ttest_v = tokenizer.coded_format([[alpha[i] for i in vgb.parse(doc) if i < len(alpha)] for doc in ttest])
-    ttrain_w = tokenizer.coded_format([tokenizer.encode(doc.split(' ')) for doc in train])
-    ttest_w = tokenizer.coded_format([tokenizer.encode(doc.split(' ')) for doc in test])
-    print("format: ", time.time() - start)
-
-    # do_ml(ttrain, train.target, ttest, test.target)
-    # print("ml: ", time.time() - start)
-
-    vectorizer_v1 = CountVectorizer()
-    vectorizer_v1.fit(ttrain_v)
-    vectorizer_w1 = CountVectorizer()
-    vectorizer_w1.fit(ttrain_w)
-
-    vectorizer_w2 = CountVectorizer(vocabulary=set(vectorizer_w1.vocabulary_.keys()) - set(vectorizer_v1.vocabulary_.keys()))
-    vec_ttrain_w = vectorizer_w2.fit_transform(ttrain_w)
-    vec_ttrain_v = vectorizer_v1.transform(ttrain_v)
-    vec_ttrain = np.hstack((vec_ttrain_w, vec_ttrain_v))
-
-    vec_ttest_w = vectorizer_w2.fit_transform(ttest_w)
-    vec_ttest_v = vectorizer_v1.transform(ttest_v)
-    vec_ttest = np.hstack((vec_ttest_w, vec_ttest_v))
-    print("vectorizer: ", time.time() - start)
-
-    pipeline = Pipeline([('tfidf', TfidfTransformer()),
-                         ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=5, random_state=42))
-                         ])
-    pipeline = pipeline.fit(vec_ttrain, train.target)
-    predicted = pipeline.predict(vec_ttest)
-    print(np.mean(predicted == test.target))
-
-    print("ml: ", time.time() - start)
-    # print(type(train))
-    # print(train.data[0])
-    # vectorizer = CountVectorizer(analyzer='char')
-    # train_char = vectorizer.fit_transform(train.data)
-    # print('##################')
-    # print(train_char[0])
-    # print('##################')
-    # print(vectorizer.decode(train.data[0]))
+    def tokenize(self, X):
+        return [[c for c in x] for x in X]
 
 
 class TokenizerP:
@@ -189,11 +115,15 @@ class TokenizerP:
         self.tok = Tokenizer()
 
     def fit(self, X, y=None):
-        self.tok.fit(X)
+        xx = [re.sub("[^\w\d]", "", x).lower() for x in X]
+        # xx = X
+        self.tok.fit(xx)
         return self
 
     def transform(self, X, y=None):
-        return self.tok.transform(X)
+        xx = [re.sub("[^\w\d]", "", x).lower() for x in X]
+        # xx = X
+        return self.tok.transform(xx)
 
 
 class VGramBuilderP:
@@ -201,51 +131,177 @@ class VGramBuilderP:
         self.vgb = VGramBuilder(size, iter_num)
 
     def fit(self, X, y=None):
+        print("in vgram builder fit")
         self.vgb.fit(X)
+        print("in vgram builder fit 2. yeah!")
         return self
 
     def transform(self, X, y=None):
-        return self.vgb.transform(X)
+        res = [" ".join([str(a) for a in x]) for x in self.vgb.transform(X)]
+        print("transform")
+        return res
+
+    def alpha(self):
+        return self.vgb.alphabet()
 
 
-def vgram_20ng():
-    train, test = fetch_20newsgroups(subset='train'), fetch_20newsgroups(subset='test')
-    # VGramFeatures = Pipeline([
-    #     ("tokenizer", TokenizerP()),
-    #     ("vgb", VGramBuilderP(10000, 1)),
-    #     ('vectorizer', CountVectorizer())
-    # ])
-    # union = FeatureUnion([("vgram", VGramFeatures), ("words", CountVectorizer)])
-    # res = union.fit_transform(train.data, train.target)
-    # print(res.shape)
-    # print(res[0])
+def only_words(X_tr, y_tr, X_te, y_te):
+    pipeline = Pipeline([
+        ("words", CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, max_iter=50, random_state=42))
+    ])
+    pipeline = pipeline.fit(X_tr, y_tr)
+    predicted = pipeline.predict(X_te)
+    print(np.mean(predicted == y_te))
 
-    # pipeline = Pipeline([
-    #     ("features", FeatureUnion([("vgram", VGramFeatures), ("words", CountVectorizer)])),
-    #     ('tfidf', TfidfTransformer()),
-    #     ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=5, random_state=42))
-    # ])
+
+def only_vgram(X_tr, y_tr, X_te, y_te):
+    vgram_features = Pipeline([
+        ("tokenizer", CharTokenizer()),
+        ("vgb", VGramBuilderP(15000, 20))
+    ])
+    data = [x for x in X_tr]
+    for x in X_te:
+        data.append(x)
+    vgram_features.fit(data)
+    traint = vgram_features.transform(X_tr)
+    testt = vgram_features.transform(X_te)
 
     pipeline = Pipeline([
-        ("tokenizer", TokenizerP()),
-        ("vgb", VGramBuilderP(10000, 1)),
-        ('vectorizer', CountVectorizer()),
+        ("words", CountVectorizer()),
         ('tfidf', TfidfTransformer()),
-        ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=5, random_state=42))
+        ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, max_iter=20, random_state=42))
     ])
-    pipeline = pipeline.fit(train.data, train.target)
-    predicted = pipeline.predict(test.data)
-    print(np.mean(predicted == test.target))
+    pipeline = pipeline.fit(traint, y_tr)
+    predicted = pipeline.predict(testt)
+    print(np.mean(predicted == y_te))
+    # alpha = vgram_features.named_steps['vgb'].alpha()
+    # alpha = vgram_features.named_steps['tokenizer'].decode(alpha)
+    # for word in alpha:
+    #     print(word)
 
+
+def vgram_and_words(X_tr, y_tr, X_te, y_te):
+    size = 5000
+    iter = 3
+    vgram_features = Pipeline([
+        ("tokenizer", CharTokenizer()),
+        ("vgb", VGramBuilderP(size, iter))
+    ])
+    data = [x for x in X_tr]
+    for x in X_te:
+        data.append(x)
+    vgram_features.fit(data)
+    # traint = vgram_features.transform(train.data)
+    # testt = vgram_features.transform(test.data)
+    all_words = CountVectorizer().fit(data)
+    words_without_vgrams = set(all_words.vocabulary_)
+    vgrams_alpha = set(vgram_features.named_steps['tokenizer'].decode(vgram_features.named_steps["vgb"].alpha()))
+    print("all words: ", len(words_without_vgrams))
+    print("intersect: ", len(words_without_vgrams & vgrams_alpha))
+    words_without_vgrams -= vgrams_alpha
+    print("words without vgrams: ", len(words_without_vgrams))
+    # set(vgram_features.named_steps['tokenizer'].decode(vgram_features.named_steps["vgb"].alpha()))
+
+    pipeline = Pipeline([
+        ("union", FeatureUnion([
+            ("vgrams", Pipeline([
+                ("tokenizer", CharTokenizer()),
+                ("vgb", VGramBuilderP(size, iter)),
+                ("count", CountVectorizer())
+            ])),
+            ("words", CountVectorizer(vocabulary=words_without_vgrams))])),
+        ('tfidf', TfidfTransformer()),
+        ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, max_iter=20, random_state=42))
+    ])
+    pipeline = pipeline.fit(X_tr, y_tr)
+    predicted = pipeline.predict(X_te)
+    print(np.mean(predicted == y_te))
+    # alpha = vgram_features.named_steps['vgb'].alpha()
+    # alpha = vgram_features.named_steps['tokenizer'].decode(alpha)
+    # for word in alpha:
+    #     print(word)
+
+
+def all_union(X_tr, y_tr, X_te, y_te):
+    vgram_features = Pipeline([
+        ("tokenizer", CharTokenizer()),
+        ("vgb", VGramBuilderP(5000, 3)),
+        ('vectorizer', CountVectorizer())
+    ])
+    pipeline = Pipeline([
+        ("features", FeatureUnion([("vgram", vgram_features), ("words", CountVectorizer())])),
+        # ("words", CountVectorizer()),
+        # ("vgram", vgram_features),
+        ('tfidf', TfidfTransformer()),
+        ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, max_iter=50, random_state=42))
+    ])
+    pipeline = pipeline.fit(X_tr, y_tr)
+    predicted = pipeline.predict(X_te)
+    print(np.mean(predicted == y_te))
+
+
+def get_data(dataset):
+    if dataset == "20ng":
+        ng_train, ng_test = fetch_20newsgroups(subset='train'), fetch_20newsgroups(subset='test')
+        X_tr, y_tr, X_te, y_te = ng_train.data, ng_train.target, ng_test.data, ng_test.target
+    elif dataset == "imdb":
+        imdb_X_tr, imdb_y_tr = imdb_data("/Users/akhvorov/data/datasets/aclImdb/train/")
+        imdb_X_te, imdb_y_te = imdb_data("/Users/akhvorov/data/datasets/aclImdb/test/")
+        X_tr, y_tr, X_te, y_te = imdb_X_tr, imdb_y_tr, imdb_X_te, imdb_y_te
+    else:
+        print("wrong dataset")
+        return -1
+    print("train data shape: ", len(X_tr), len(X_tr[0]), len(y_tr))
+    print("test data shape: ", len(X_te), len(X_te[0]), len(y_te))
+    return X_tr, y_tr, X_te, y_te
+
+
+def apply_model(X_tr, y_tr, X_te, y_te, mode):
+    if mode == "v":
+        only_vgram(X_tr, y_tr, X_te, y_te)
+    elif mode == "w":
+        only_words(X_tr, y_tr, X_te, y_te)
+    elif mode == "w|v" or mode == "v|w":
+        all_union(X_tr, y_tr, X_te, y_te)
+    elif mode == "w&v" or mode == "v&w":
+        vgram_and_words(X_tr, y_tr, X_te, y_te)
+    else:
+        print("wrong mode")
+    # test_20ng()
+    # restore_test()
+    # all_union(X_tr, y_tr, X_te, y_te)
+    # only_vgram(X_tr, y_tr, X_te, y_te)
+    # vgram_and_words(X_tr, y_tr, X_te, y_te)
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-d', '--dataset')
+    parser.add_argument ('-m', '--mode')
+    # parser.add_argument ('-p', '--path', default='.')
+    # parser.add_argument ('-d', '--dataset', default='20newsgroups')
+    # parser.add_argument ('-n', '--size', default=15000)
+    # parser.add_argument ('-s', '--sparse', default=True)
+    # parser.add_argument ('-r', '--restored', default=False)
+    # parser.add_argument ('-v', '--use_vgrams', default=True)
+    # parser.add_argument ('-w', '--use_words', default=False)
+    return parser
 
 
 def main():
+    args = arg_parser().parse_args()
     start = time.time()
-    #test_20ng()
-    #restore_test()
-    vgram_20ng()
+    X_tr, y_tr, X_te, y_te = get_data(args.dataset)
+    apply_model(X_tr, y_tr, X_te, y_te, args.mode)
     print("time: ", time.time() - start)
 
 
 if __name__ == "__main__":
     main()
+
+
+'''
+1. 0.8614 - words + vgrams(25000, 20), double intersection, train on train, svm(1e-4, max_iter=50)
+'''
